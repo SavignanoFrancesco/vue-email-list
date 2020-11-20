d20673f83d219c8c2f2a51178e863cd3bf19fc41
Francesco <francesco.savignano96@gmail.com>
2020-11-20 16:01:48 +0100

    aggiunto controllo in modo che non ci siano email uguali e variabile email_quantity per decidere quante generarne

--- main.js
+++ main.js
@@ -3,17 +3,21 @@ var app = new Vue({
     el: '#root',
     data: {
         email: [],
+        email_quantity: 10,
     },
 
     mounted() {
         const self = this;
-        for (var i = 0; i < 10; i++) {
+        for (var i = 0; i < self.email_quantity; i++) {
             axios
                 .get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then(function(risposta) {
 
                     // console.log(risposta);
-                    self.email.push(risposta.data.response);
+
+                    if (!self.email.includes(risposta.data.response)) {
+                        self.email.push(risposta.data.response);
+                    }
 
                 });
         }
