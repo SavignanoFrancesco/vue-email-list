5528f301fe5e60a25317001e6878a59b0a1a61f1
Francesco <francesco.savignano96@gmail.com>
2020-11-20 15:07:34 +0100

    generazione di 10 email tramite ciclo for

--- main.js
+++ main.js
@@ -2,17 +2,22 @@
 var app = new Vue({
     el: '#root',
     data: {
-        email: false,
+        email: [],
     },
 
     mounted() {
         const self = this;
-        axios
-            .get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(function(risposta) {
-                console.log(risposta);
-                self.email = risposta.data.response;
-            });
+        for (var i = 0; i < 10; i++) {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(function(risposta) {
 
+                    console.log(risposta);
+                    // self.email = risposta.data.response;
+
+                    self.email.push(risposta.data.response);
+
+                });
+        }
     }
 });
